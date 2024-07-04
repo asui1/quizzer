a5bfb1675a552418a979cd8f16b5a7a97fc979cd
asui1 <whwkd122@kaist.ac.kr>
2024-07-04 23:31:30 +0900

    quizWidgetGenerator3 fixed

--- lib/Widgets/Generator/quizWidget3Generator.dart
+++ lib/Widgets/Generator/quizWidget3Generator.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:quizzer/Class/quiz3.dart';
 import 'package:quizzer/Class/quizLayout.dart';
 import 'package:quizzer/Widgets/GeneratorCommon.dart';
+import 'package:quizzer/Widgets/ViewerCommon.dart';
 import 'package:quizzer/config.dart';
 
 class QuizWidget3 extends StatefulWidget {
@@ -41,109 +42,148 @@ class _QuizWidget3State extends State<QuizWidget3> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(AppConfig.padding),
-        child: Column(
-          children: <Widget>[
-            questionInputTextField(
-              controller: questionController,
-              onChanged: (value) {
-                widget.quiz.setQuestion(value);
-              },
-              quizLayout: widget.quizLayout,
-            ),
-            SizedBox(height: AppConfig.padding),
-            Column(
-              children: <Widget>[
-                for (int index = 0;
-                    index < widget.quiz.getAnswersLength();
-                    index++) ...[
-                  Row(
-                    children: [
-                      SizedBox(height: AppConfig.padding),
-                      Expanded(
-                        child: TextField(
-                            key: ValueKey(index), // TextField에 대한 Key
-                            controller: _controllers[index],
-                            decoration: InputDecoration(
-                              enabledBorder: OutlineInputBorder(
-                                // 활성화되지 않았을 때의 테두리 스타일
-                                borderSide: BorderSide(
-                                  color: index == 0 ? widget.quizLayout.getBorderColor2() : widget.quizLayout.getBorderColor1(), // 테두리 색상 변경
-                                  width: 2.0, // 테두리 두께 변경
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        body: SafeArea(
+          child: Container(
+            height: AppConfig.screenHeight,
+            width: AppConfig.screenWidth,
+            decoration: backgroundDecoration(quizLayout: widget.quizLayout),
+            child: Stack(
+              children: [
+                Positioned.fill(
+                  child: Padding(
+                    padding: EdgeInsets.all(AppConfig.padding),
+                    child: Column(
+                      children: <Widget>[
+                        questionInputTextField(
+                          controller: questionController,
+                          onChanged: (value) {
+                            widget.quiz.setQuestion(value);
+                          },
+                          quizLayout: widget.quizLayout,
+                        ),
+                        SizedBox(height: AppConfig.padding),
+                        Expanded(
+                          child: SingleChildScrollView(
+                            child: Column(
+                              children: <Widget>[
+                                for (int index = 0;
+                                    index < widget.quiz.getAnswersLength();
+                                    index++) ...[
+                                  Row(
+                                    children: [
+                                      SizedBox(height: AppConfig.padding),
+                                      Expanded(
+                                        child: TextField(
+                                            key: ValueKey(
+                                                index), // TextField에 대한 Key
+                                            controller: _controllers[index],
+                                            decoration: InputDecoration(
+                                              enabledBorder: OutlineInputBorder(
+                                                // 활성화되지 않았을 때의 테두리 스타일
+                                                borderSide: BorderSide(
+                                                  color: index == 0
+                                                      ? widget.quizLayout
+                                                          .getBorderColor2()
+                                                      : widget.quizLayout
+                                                          .getBorderColor1(), // 테두리 색상 변경
+                                                  width: 2.0, // 테두리 두께 변경
+                                                ),
+                                                borderRadius:
+                                                    BorderRadius.circular(
+                                                        10), // 테두리 모서리 둥글기 변경
+                                              ),
+                                              focusedBorder: OutlineInputBorder(
+                                                // 포커스를 받았을 때의 테두리 스타일
+                                                borderSide: BorderSide(
+                                                  color: widget.quizLayout
+                                                      .getSelectedColor(), // 테두리 색상 변경
+                                                  width: 2.5, // 테두리 두께 변경
+                                                ),
+                                                borderRadius:
+                                                    BorderRadius.circular(
+                                                        10), // 테두리 모서리 둥글기 변경
+                                              ),
+                                              hintText: '답변 ${index + 1}',
+                                            ),
+                                            onChanged: (value) {
+                                              setState(() {
+                                                widget.quiz
+                                                    .setAnswerAt(index, value);
+                                              });
+                                            },
+                                            style: widget.quizLayout
+                                                .getAnswerTextStyle()),
+                                      ),
+                                      IconButton(
+                                        icon: Icon(Icons.remove_circle_outline),
+                                        color:
+                                            widget.quizLayout.getButtonColor(),
+                                        onPressed: () {
+                                          setState(() {
+                                            widget.quiz.removeAnswerAt(index);
+                                            _controllers.removeAt(
+                                                index); // 컨트롤러 리스트에서도 해당 항목 제거
+                                          });
+                                        },
+                                      ),
+                                    ],
+                                  ),
+                                  if (index <
+                                      widget.quiz.answers.length -
+                                          1) // 마지막 TextField 다음에는 아이콘을 추가하지 않음
+                                    IgnorePointer(
+                                      child: Padding(
+                                        padding: const EdgeInsets.symmetric(
+                                            vertical: 16.0),
+                                        child: Align(
+                                          alignment: Alignment.center,
+                                          child: Icon(
+                                            Icons.arrow_downward,
+                                            color: widget.quizLayout
+                                                .getButtonColor(),
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                ],
+                                SizedBox(height: 20.0),
+                                ElevatedButton(
+                                  child: Icon(Icons.add),
+                                  onPressed: () {
+                                    setState(() {
+                                      widget.quiz.addAnswer('');
+                                      _controllers.add(TextEditingController());
+                                    });
+                                  },
                                 ),
-                                borderRadius:
-                                    BorderRadius.circular(10), // 테두리 모서리 둥글기 변경
-                              ),
-                              focusedBorder: OutlineInputBorder(
-                                // 포커스를 받았을 때의 테두리 스타일
-                                borderSide: BorderSide(
-                                  color: widget.quizLayout.getSelectedColor(), // 테두리 색상 변경
-                                  width: 2.5, // 테두리 두께 변경
+                                SizedBox(
+                                  height: AppConfig.padding * 6,
                                 ),
-                                borderRadius:
-                                    BorderRadius.circular(10), // 테두리 모서리 둥글기 변경
-                              ),
-                              hintText: '답변 ${index + 1}',
+                              ],
                             ),
-                            onChanged: (value) {
-                              setState(() {
-                                widget.quiz.setAnswerAt(index, value);
-                              });
-                            },
-                            style: widget.quizLayout.getAnswerTextStyle()),
-                      ),
-                      IconButton(
-                        icon: Icon(Icons.remove_circle_outline),
-                        color: widget.quizLayout.getButtonColor(),
-                        onPressed: () {
-                          setState(() {
-                            widget.quiz.removeAnswerAt(index);
-                            _controllers
-                                .removeAt(index); // 컨트롤러 리스트에서도 해당 항목 제거
-                          });
-                        },
-                      ),
-                    ],
-                  ),
-                  if (index <
-                      widget.quiz.answers.length -
-                          1) // 마지막 TextField 다음에는 아이콘을 추가하지 않음
-                    IgnorePointer(
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 16.0),
-                        child: Align(
-                          alignment: Alignment.center,
-                          child: Icon(
-                            Icons.arrow_downward,
-                            color: widget.quizLayout.getButtonColor(),
                           ),
                         ),
-                      ),
+                      ],
                     ),
-                ],
+                  ),
+                ),
+                Positioned(
+                  right: AppConfig.padding, // Adjust the position as needed
+                  bottom: AppConfig.padding, // Adjust the position as needed
+                  child: GeneratorDoneButton(
+                    onPressed: () {
+                      Navigator.pop(context, widget.quiz);
+                    },
+                  ),
+                ),
               ],
             ),
-            SizedBox(height: 20.0),
-            ElevatedButton(
-              child: Icon(Icons.add),
-              onPressed: () {
-                setState(() {
-                  widget.quiz.addAnswer('');
-                  _controllers.add(TextEditingController());
-                });
-              },
-            ),
-            Spacer(
-              flex: 1,
-            ),
-            GeneratorDoneButton(
-              onPressed: () {
-                Navigator.pop(context, widget.quiz);
-              },
-            ),
-          ],
+          ),
         ),
       ),
     );
